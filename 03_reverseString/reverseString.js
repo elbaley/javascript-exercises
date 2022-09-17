07d72d22c8d76e35033a3c47129e907242202046
elbaley <lebafurkan@outlook.com>
2022-09-17 20:42:57 +0300

    add solution 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // split into chars
+  const letters = string.split("");
+  let result = "";
+  for (let i = letters.length - 1; i >= 0; i--) {
+    //add it to result string
+    result += letters[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
