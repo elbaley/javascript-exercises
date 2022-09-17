5f1157a9f6c53a34795af64b57675264965f1fe2
elbaley <lebafurkan@outlook.com>
2022-09-18 01:50:06 +0300

    add solution 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (startingNum, finalNum) {
+  let result = 0;
+  //check which number is larger
+  if (startingNum > finalNum) {
+    const tempNum = startingNum;
+    startingNum = finalNum;
+    finalNum = tempNum;
+  }
+  //throw error if either number is negative or is not an integer
+  if (
+    startingNum < 0 ||
+    finalNum < 0 ||
+    !Number.isInteger(startingNum) ||
+    !Number.isInteger(finalNum)
+  ) {
+    return "ERROR";
+  }
 
+  for (let i = startingNum; i <= finalNum; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
